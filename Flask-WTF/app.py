8b1907aee038d6ea58bd79651782198a1f172ebf
Anh Nguyen <nganhh014@gmail.com>
2021-11-19 08:52:31 +0200

    update code

--- Flask-WTF/app.py
+++ Flask-WTF/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, BooleanField
+from wtforms import StringField, PasswordField, IntegerField, BooleanField, Form, FormField
 from wtforms.validators import InputRequired, length, Email
+
+class TelephoneForm(Form):
+    country_code = IntegerField('country code')
+    area_code = IntegerField('area code')
+    number = StringField('number')
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[
@@ -17,6 +22,12 @@ class LoginForm(FlaskForm):
         Email(),
     ])
 
+class NameForm(LoginForm):
+    first_name = StringField('first name', validators=[InputRequired(),])
+    last_name = StringField('last name', validators=[InputRequired(),])
+    home_phone = FormField(TelephoneForm)
+    mobile_phone = FormField(TelephoneForm)
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Mysecret!'
@@ -24,10 +35,12 @@ def create_app():
 
     @app.route('/', methods=['GET', 'POST'])
     def index():
-        form = LoginForm()
+        form = NameForm()
 
         if form.validate_on_submit():
-            return f'<h1>Username: {form.username.data} Password: {form.password.data} Age: {form.age.data}</h1>'
+
+            return f'Country Code: {form.mobile_phone.country_code.data} Area Code: {form.home_phone.area_code.data} Number: {form.home_phone.number.data}'
+            #return f'<h1>Username: {form.username.data} Password: {form.password.data} Age: {form.age.data}</h1>'
 
         return render_template('index.html', form=form)
 
